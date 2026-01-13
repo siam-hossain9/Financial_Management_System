676a8d8260890cefd887b843d0e8126bb0f2e012
siam-hossain9 <siamhossain1130@gmail.com>
2026-01-14 01:22:20 +0600

    new upload

---
+++ layouts/views/home_view.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- upper section -->
+<div class="upper-section">
+    <?php include "./header.php"; ?>
+
+    <div class="left-upper-section" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+        
+        <h1 class="upper-title">
+            Best Way <span class="title-word-color-black">To</span> <br>
+            <span class="title-word-color-black">Save your</span> <br> Money
+        </h1>
+       
+        <a href="./services_view.php" class="get-started-btn">Get Started</a>
+       
+    </div>
+</div>
+
+
+<!-- middle section  -->
+
+<div class="middle-section" data-aos="fade-up" data-aos-duration="1000">
+    <h1 class="middle-title">Why Choose Us?</h1>
+    <p class="middle-sub-title">
+        We help you save smarter by offering simple, reliable solutions that actually make a difference.
+    </p>
+
+    <div class="middle-sub-section" data-aos="fade-right" data-aos-duration="1000">
+        <img class="middle-img-1" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" src="../assets/middle-img-1.png" alt="" srcset="">
+        <p class="middle-description-1">
+            Our tools are designed to be easy to use and effective, helping you stay in control of your money without unnecessary complexity.
+        </p>
+    </div>
+
+
+    <div class="middle-sub-section" data-aos="fade-down" data-aos-duration="1000">
+        <img class="middle-img-2" src="../assets/middle-img-2.png" alt="home page background" srcset="">
+        <p class="middle-description-2">
+            We focus on transparency and trust, so you always know where your money is going and how it’s working for you.
+        </p>
+    </div>
+
+
+    <div class="middle-sub-section" data-aos="fade-left" data-aos-duration="1000">
+        <img class="middle-img-3" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000" src="../assets/middle-img-3.png" alt="" srcset="">
+        <p class="middle-description-3">
+            With smart planning and practical features, we help you build better habits and reach your financial goals faster.
+        </p>
+    </div>
+</div>
+
+
+<!-- animation on scroll js  -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
+<?php include "./footer.php" ?>
+</body>
+</html>
